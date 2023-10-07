96796ee132a28f4b74dda709a8e3cbe65d51515b
Alexandre Donadel <xanded.fer2@gmail.com>
2023-10-07 16:44:57 -0300

    Corrigindo problemas gerais

--- src/main/java/com/example/ecommerce/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/example/ecommerce/ecommerce/controller/UsuarioController.java
@@ -3,13 +3,17 @@ package com.example.ecommerce.ecommerce.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ecommerce.ecommerce.model.UsuarioEntity;
 import com.example.ecommerce.ecommerce.repository.UsuarioRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
 public class UsuarioController {
     @Autowired
     UsuarioRepository usuarioRepository;
