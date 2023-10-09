2037b8a1205e9dea134d91b041511c939ceb0da2
Alexandre Donadel <xanded.fer2@gmail.com>
2023-10-09 20:57:05 -0300

    Primeira alterações de add, delete e update

--- src/main/java/com/example/ecommerce/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/example/ecommerce/ecommerce/controller/UsuarioController.java
@@ -1,10 +1,15 @@
 package com.example.ecommerce.ecommerce.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,16 +23,25 @@ public class UsuarioController {
     @Autowired
     UsuarioRepository usuarioRepository;
 
-    @GetMapping(value="/")
-    public void get() 
+    @GetMapping(value="/usuario/listar")
+    public List<UsuarioEntity> getMethodName() 
     {
-
+        return usuarioRepository.findAll();
     }
 
     @PostMapping (value="/usuario")
     public ResponseEntity<UsuarioEntity> salvar(@RequestBody UsuarioEntity usuario)
     {
+        usuario.createdAt = LocalDateTime.now();
+        usuario.updatedAt = LocalDateTime.now();
         UsuarioEntity _user = usuarioRepository.save(usuario);
         return new ResponseEntity<>(_user, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping (path = {"/usuario{id}"})
+    public ResponseEntity <?> excluir(@PathVariable long id)
+    {
+        usuarioRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 }
